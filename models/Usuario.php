72e572c7c71aa4271471695f8a1bb317a47a6a4a
nicolasb <nicolasbotello@hotmail.com>
2023-10-24 17:02:10 -0500

    validando creacion de usuarios

--- models/Usuario.php
+++ models/Usuario.php
@@ -8,8 +8,6 @@ class Usuario extends ActiveRecord{
     protected static $tabla = 'usuarios';
     protected static $columnasDB = ['id','nombre','apellido','imagen','email','password','telefono','admin','confirmado','token'];
 
-    public static $e
-
     public $id;
     public $nombre;
     public $apellido;
@@ -37,8 +35,32 @@ class Usuario extends ActiveRecord{
     // Mensajes de validacion para la creacion de una cuenta
     public function validarNuevaCuenta(){
         if(!$this->nombre){
-            self::$alertas['error'] = 'El nombre es obligatorio';
+            self::$alertas[] = 'El nombre es obligatorio';
+        }else if (! preg_match("/^[a-zA-Z ]+$/", $this->nombre)){
+            self::$alertas[] = 'El nombre no puede llevar numeros o caraceteres especiales';
         }
 
+        if(!$this->apellido){
+            self::$alertas[] = 'El Apellido es obligatorio';
+        }else if (! preg_match("/^[a-zA-Z ]+$/", $this->apellido)){
+            self::$alertas[] = 'El Apellido no puede llevar numeros o caraceteres especiales';
+        }
+        
+        // No es obligatorio el telefono, pero si debe tener el formato de 10 numeros
+        if(!empty($this->telefono)  && !preg_match("/^[0-9]{10}$/", $this->telefono)){
+            self::$alertas[] = 'El numero de telefono solo puede tener 10 numeros';
+        }
+
+        if( !empty($this->telefono)  && !filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            self::$alertas[] = 'El correo tiene un formato invalido';
+        }
+
+        if(!$this->password){
+            self::$alertas[] = 'La contraseña es obligatoria';
+        }else if( !preg_match("/^(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){5,})(?=(?:.*[0-9]){1})/", $this->password)){
+            self::$alertas[] = "La contraseña no es válida. Debe contener al menos 5 letras minúsculas, un número y una letra mayúscula.";
+        }
+
+        return self::$alertas;
     }
 }
