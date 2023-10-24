80947733c5578b4afcf33aa5ed72c36dfdeeb0f1
nicolasb <nicolasbotello@hotmail.com>
2023-10-24 18:20:33 -0500

    creo el template de alertas

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -27,12 +27,15 @@ class LoginController{
     
     public static function crear(Router $router){
         $usuario = new Usuario();
+        $alertas=[];
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             $usuario->sincronizar($_POST);
             $alertas = $usuario->validarNuevaCuenta();
+            // debuguear($alertas);
         }
         $router->render('auth/crear-cuenta',[
-            "usuario" => $usuario
+            "usuario" => $usuario,
+            "alertas"=> $alertas
         ]);
     }
 }
--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -1,6 +1,10 @@
 <h1 class="nombre-pagina">Crear cuenta</h1>
 <p class="descripcion-pagina">Llena el siguiente formulario para crear una cuenta</p>
 
+<?php
+    include_once __DIR__ . "/../templates/alertas.php";
+?>
+
 <form class="formulario" method="post" action="/crear-cuenta">
     <div class="campo">
         <label for="nombre">Nombre</label>
