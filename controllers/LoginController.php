593072620bf4a1ed2a6357f80383e1c9ff5675d5
nicolasb <nicolasbotello@hotmail.com>
2023-10-23 21:49:24 -0500

    creando la vista basica de login

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,10 +2,12 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController{
 
-    public static function login(){
-        echo 'desde login';
+    public static function login(Router $router){
+        $router->render('auth/login');
     }
 
     public static function logout(){
