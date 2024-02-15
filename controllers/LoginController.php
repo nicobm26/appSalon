b38015234bc085c30a4c68e8bbf35ba931d8af01
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-15 08:49:26 -0500

    enviar correo con instrucciones

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -81,6 +81,10 @@ class LoginController{
                     $usuario->guardar();
 
                     //Enviar el correo
+                    $email = new Email($usuario->email, $usuario->nombre ,$usuario->token);
+                    $email->enviarInstrucciones();
+
+                    //Alerta de exito
                     Usuario::setAlerta('exito','Revista tu email');
 
                     // debuguear($usuario);
