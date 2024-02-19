f2eb0d29229c31b9a8e88b207afbfbbfe8177cf3
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-19 14:42:19 -0500

    creo el apicontroller que servirar para traer los datos

---
+++ controllers/APIController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+use Model\Servicio;
+
+class APIController{
+
+    public static function index(){
+        $servicios = Servicio::all();
+        // debuguear($servicios);
+        echo json_encode($servicios);
+    }
+
+}
