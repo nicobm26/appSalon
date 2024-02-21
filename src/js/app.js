167c06b8ed6629ae8c3142b4a56b250ccd828aaa
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-21 09:21:51 -0500

    seleccionando servicios para la cita

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,13 @@ let paso = 1;
 const pasoInicial=1;
 const pasoFinal=3;
 
+const cita= {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     iniciarApp();
 })
@@ -115,12 +122,23 @@ function mostrarServicios(servicios){
         const servicioDiv = document.createElement('DIV');
         servicioDiv.classList.add('servicio');
         servicioDiv.dataset.idServicio = id;
+        
+        servicioDiv.onclick = function(){
+            seleccionarServicio(servicio);
+            //callback
+        }
 
         servicioDiv.appendChild(nombreServicio);
         servicioDiv.appendChild(precioServicio);
 
         document.querySelector('#servicios').appendChild(servicioDiv);
-    });
-    
+    });    
+}
 
+function seleccionarServicio(servicio){
+   
+    const {servicios} = cita;    
+    cita.servicios = [...servicios, servicio];
+
+    console.log(cita.servicios);
 }
