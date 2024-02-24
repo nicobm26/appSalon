45b875564ca5fd22cd5d3ada784d120229ed545b
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-24 13:06:50 -0500

    agrego una clase al div que contiene el resumen

--- views/cita/index.php
+++ views/cita/index.php
@@ -48,7 +48,7 @@
         </div>  
     </div>
 
-    <div id="paso-3" class="seccion">
+    <div id="paso-3" class="seccion contenido-resumen">
         <h2>Resumen</h2>
         <p>Verifica que la información sea correcta</p>
     </div>
