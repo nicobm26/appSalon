ec658ba1d5a8e125b0bd638d6d3709cbb592c9d5
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-27 15:21:52 -0500

    inserto el script para usar en esta vista la bibiblioteca de sweetAlarm

--- views/cita/index.php
+++ views/cita/index.php
@@ -72,7 +72,7 @@
 <?php
 
     $script = "
-    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
     <script src='build/js/app.js'> </script> 
     ";
 ?>
