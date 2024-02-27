0504b370f4654d938eda09077fe5719446bff5c7
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-26 20:10:19 -0500

    metodo guardar del apicontroller

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Cita;
 use Model\Servicio;
 
 class APIController{
@@ -10,6 +11,12 @@ class APIController{
         $servicios = Servicio::all();
         // debuguear($servicios);
         echo json_encode($servicios);
+    }
+
+    public static function guardar(){
+        $cita = new Cita($_POST);
+        $resultado = $cita->guardar();
+        echo json_encode($resultado);
     }
 
 }
