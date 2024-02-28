27aa0fb084b16bd248ef337e6cb33b7f02dd9791
nicolasb <nicolasbotellomendoza@gmail.com>
2024-02-28 10:09:29 -0500

    muestro las citas en la vista

--- views/admin/index.php
+++ views/admin/index.php
@@ -18,5 +18,23 @@
 </div>
 
 <div class="citas-admin">
+    <ul class="citas">
+    <?php $idCita = 0;?>
+    <?php foreach($citas as $cita): ?>        
+        <?php if($idCita !== $cita->id): ?>
+            <li>
+                <p>ID: <span><?php echo $cita->id; ?></span></p>
+                <p>HORA: <span><?php echo $cita->hora; ?></span></p>
+                <p>CLIENTE: <span><?php echo $cita->cliente; ?></span></p>
+                <p>EMAIL: <span><?php echo $cita->email; ?></span></p>
+                <p>TELEFONO: <span><?php echo $cita->telefono; ?></span></p>
 
+                <h3>Servicios</h3>
+                <?php $idCita = $cita->id ?>
+        <?php endif ?>
+        <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio ?></p>
+            </li>
+
+    <?php endforeach ?>
+    </ul>
 </div>
