8a68aff2a0598caa43d41fb89a90cdc78d582c52
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-01 18:02:32 -0500

    pequeño cambio

--- views/admin/index.php
+++ views/admin/index.php
@@ -11,17 +11,25 @@
             <input 
                 type="date"
                 id="fecha"
-                name="fecha"               
+                name="fecha" 
+                value="<?php echo $fecha; ?>"              
             />
         </div>
     </form>
 </div>
 
+<?php
+    if(count($citas) === 0) {
+        echo "<h2>No hay citas en esta fecha</h2>";
+    }
+?>
+
 <div class="citas-admin">
     <ul class="citas">
     <?php $idCita = 0;?>
-    <?php foreach($citas as $cita): ?>        
+    <?php foreach($citas as $key => $cita): ?>        
         <?php if($idCita !== $cita->id): ?>
+            <?php   $total = 0;   ?>
             <li>
                 <p>ID: <span><?php echo $cita->id; ?></span></p>
                 <p>HORA: <span><?php echo $cita->hora; ?></span></p>
@@ -32,9 +40,23 @@
                 <h3>Servicios</h3>
                 <?php $idCita = $cita->id ?>
         <?php endif ?>
-        <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio ?></p>
-            </li>
+        <?php 
+            $total += $cita->precio;
+            ?>
+            <p class="servicio"><?php echo $cita->servicio . " " . number_format($cita->precio, 0 , ',', '.') ?></p>
+            <?php
 
+            $actual = $cita->id;
+            $proximo = $citas[$key+1]->id ?? 0;
+
+            if(esUltimo($actual, $proximo)){ ?>
+                <p class="total">Total: <span> <?php echo number_format($total, 0, ',', '.'); ?></span></p>
+            
+            <?php
+            }
+        ?>
+       
+            
     <?php endforeach ?>
     </ul>
 </div>
