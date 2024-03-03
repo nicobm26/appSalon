3bd4397805a3c1a31dd96860eb6cb6811b4e6c54
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-02 20:09:40 -0500

    arregle el error, toca ponerlo sin espacio. tal cual como lo acabe de modificar

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,7 +1,7 @@
 <h1 class="nombre-pagina">Crear nueva cita</h1>
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
-<?php include_once __DIR__.' /../templates/barra.php';?>
+<?php include_once __DIR__.'/../templates/barra.php';?>
 
 <div id="app">
     <div class="tabs">
