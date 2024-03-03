f2496c25b33ccb1edc4f442e828c25fb487109eb
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-02 20:43:08 -0500

    formulario para crear o actualizar servicio

---
+++ views/servicios/formulario.php
@@ -0,0 +1,19 @@
+<div class="campo">
+    <label for="nombre">Nombre</label>
+    <input 
+        type="text"
+        id="nombre"
+        name="nombre"
+        placeholder="Nombre Servicio"
+    />
+</div>
+
+<div class="campo">
+    <label for="precio">Precio</label>
+    <input 
+        type="number"
+        id="precio"
+        name="precio"
+        placeholder="Precio Servicio"
+    />
+</div>
