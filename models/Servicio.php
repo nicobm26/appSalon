5e62f8d1c1973199b7d4446a1f8fea05443d1d15
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-02 21:03:12 -0500

    metodo para validar

--- models/Servicio.php
+++ models/Servicio.php
@@ -15,4 +15,18 @@ class Servicio extends ActiveRecord{
         $this->nombre =  $args['nombre']?? '';
         $this->precio =  $args['precio'] ?? '';
     }
+
+    public function validar() : Array{
+        if(!$this->nombre){
+            self::$alertas['error'][] = 'El nombre del servicio es obligatorio';
+        }
+        if(!$this->precio){
+            self::$alertas['error'][] = 'EL precio es obligatorio';
+        }
+        if( ! is_numeric($this->precio) ){
+            self::$alertas['error'][] = 'El precio no es valido';
+        }
+
+        return self::$alertas;
+    }
 }
