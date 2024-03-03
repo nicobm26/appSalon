e11ece5b04e617f78c3654366337f25f1beff907
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-02 21:03:31 -0500

    se ignora la barra

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -1,7 +1,9 @@
 <h1 class="nombre-pagina">Nuevo Servicio</h1>
 <p class="descripcion-pagina">LLena todos los campos para añadir un nuevo servicio</p>
 <?php
-    include_once __DIR__ . '/../templates/barra.php';
+    // include_once __DIR__ . '/../templates/barra.php';
+    include_once __DIR__ . '/../templates/alertas.php'; 
+
 ?>
 
 <form action="/servicios/crear" method="post">
