3714897a0bbad34557a0d37593a54cf5d6b25881
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-02 20:43:34 -0500

    vista de actualizar servicio

---
+++ views/servicios/actualizar.php
@@ -0,0 +1,11 @@
+<h1 class="nombre-pagina">actualizar Servicio</h1>
+<p class="descripcion-pagina">Modifica los valores del formulario</p>
+<?php
+    include_once __DIR__ . '/../templates/barra.php';
+?>
+
+<form action="/servicios/actualizar" method="post">
+    <?php include_once __DIR__ . "/formulario.php"; ?>
+    
+    <input type="submit" class="boton" value="Actualizar Servicio">
+</form>
