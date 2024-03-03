cca32295a520c5f5611fc8e1f627db259132e9e0
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-02 19:06:35 -0500

    servicios del admin y estilos

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -4,3 +4,18 @@
     <a class="boton" href="/logout">Cerrar Sesión</a>
 
 </div>
+
+<?php
+
+if(isset($_SESSION['admin'])){
+?>
+<div class="barra-servicios">
+    <a class="boton" href="/admin">Ver Citas</a>
+    <a class="boton" href="/servicios">Ver servicios</a>
+    <a class="boton" href="/servicios/crear">Nuevo Servicio</a>
+</div>
+
+<?php
+
+}
+?>
