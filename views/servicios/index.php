14e9e3ed3b3026c81f13aaf4007fe48737b1e706
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-04 10:14:58 -0500

    agregar js para preguntar si esta seguro de eliminar

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -12,11 +12,24 @@
         </li>
         <div class="acciones">
             <a href="/servicios/actualizar?id=<?php echo $servicio->id?>" class="boton">Actualizar</a>
-            <form action="/servicios/eliminar" method="POST">
+            <form action="/servicios/eliminar" method="POST" id="formEliminarServicio-<?php echo $servicio->id; ?>">
                 <input type="hidden" name="id" value="<?php echo $servicio->id?>">
-                <input type="submit" value="Borrar" class="boton-eliminar">
+                <input 
+                    type="submit" 
+                    value="Borrar" 
+                    class="boton-eliminar" 
+                    onclick="confirmDelete(event,'formEliminarServicio-<?php echo $servicio->id; ?>')"
+                />
             </form>
         </div>
 
     <?php endforeach ?>
 </ul>
+
+<?php
+    $script = "
+    <script src='//cdn.jsdelivr.net/npm/sweetalert2@10'></script>
+    <script src='/build/js/alertas.js'></script>
+    <script src='https://kit.fontawesome.com/d74a8aa5fa.js' crossorigin='anonymous'></script>
+    ";
+?>
