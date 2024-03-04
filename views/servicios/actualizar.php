b28d8a46180ce47fdbe87d71ba826eb9141cced6
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-04 09:15:18 -0500

    elimino el action, ya que es para la misma url

--- views/servicios/actualizar.php
+++ views/servicios/actualizar.php
@@ -4,7 +4,7 @@
     include_once __DIR__ . '/../templates/barra.php';
 ?>
 
-<form action="/servicios/actualizar" method="post">
+<form method="post">
     <?php include_once __DIR__ . "/formulario.php"; ?>
     
     <input type="submit" class="boton" value="Actualizar Servicio">
