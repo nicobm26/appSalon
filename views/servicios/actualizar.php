403dfd1ae45660456c77bc821b075bab6ad55ac6
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-04 09:52:44 -0500

    agrego un evento al boton para hacerlo mas dinamico

--- views/servicios/actualizar.php
+++ views/servicios/actualizar.php
@@ -2,10 +2,25 @@
 <p class="descripcion-pagina">Modifica los valores del formulario</p>
 <?php
     include_once __DIR__ . '/../templates/barra.php';
+    include_once __DIR__ . '/../templates/alertas.php';
 ?>
 
 <form method="post">
     <?php include_once __DIR__ . "/formulario.php"; ?>
     
-    <input type="submit" class="boton" value="Actualizar Servicio">
+    <input 
+        type="submit" 
+        class="boton" 
+        value="Actualizar Servicio"
+        onclick="servicioActualizado(event)"
+    />
 </form>
+
+<?php
+
+$script = "
+    <script src='//cdn.jsdelivr.net/npm/sweetalert2@10'></script>
+    <script src='/build/js/alertas.js'></script>
+    <script src='https://kit.fontawesome.com/d74a8aa5fa.js' crossorigin='anonymous'></script>
+";
+?>
